0874cda07b4cca8aeedf6a052b73b45c580fe889
Chris Konstad <chriskon149@gmail.com>
2016-02-06 15:15:20 -0800

    Touched up options page, now selected subreddits can be removed

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 * [ ] add fuzzy matching
 * [ ] don't save a subreddit if it doesn't exist (URL is a search page)
 * [x] don't save data when in incognito, per google guidelines
-* [ ] be able to remove select subreddits from list
+* [x] be able to remove select subreddits from list
 * [x] be able to clear data
 * [x] make options page nicer
 * [ ] reset the counts on a schedule (weekly?), but keep the subreddit listed
--- options.js
+++ options.js
@@ -1,6 +1,67 @@
 function clear_subreddits() {
-  chrome.storage.sync.clear();
+  // TODO ask for user confirmation, but confirm doesn't work on Options2
+  chrome.storage.sync.clear(function() { loadData(); });
+}
+
+function remove_subreddits() {
+  var items = document.getElementById("subreddit_list").childNodes;
+  var toRemove = [];
+
+  for(var i=0; i< items.length; i++) {
+    var cb = items[i].childNodes[0];
+    if(cb.checked) {
+      toRemove.push(cb.value);
+    }
+  }
+
+  chrome.storage.sync.remove(toRemove);
+
+  // Refresh the listing
+  loadData();
 }
 
 document.getElementById("clear_subreddits").addEventListener("click",
   clear_subreddits);
+
+document.getElementById("remove_subreddits").addEventListener("click",
+  remove_subreddits);
+
+function loadData() {
+  var div = document.getElementById("subreddit_list");
+
+  // Empty the div
+  while(div.firstChild) {
+    div.removeChild(div.firstChild);
+  }
+
+  // Load the stored subreddits
+  chrome.storage.sync.get(null, function(items) {
+    var subs = [];
+    for(var key in items) {
+      subs.push(key);
+    }
+
+    subs.sort();
+
+    subs.forEach(function(key) {
+      var item = document.createElement("div");
+      item.className = "subreddit_item";
+      var id = "subreddit_" + key;
+      var cb = document.createElement("input");
+      cb.type = "checkbox";
+      cb.name = "subreddit";
+      cb.value = key;
+      cb.id = id;
+
+      var label = document.createElement("label");
+      label.htmlFor = id;
+      label.appendChild(document.createTextNode(key));
+
+      item.appendChild(cb);
+      item.appendChild(label);
+      div.appendChild(item);
+    });
+  });
+}
+
+loadData();
